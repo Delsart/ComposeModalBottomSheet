7cb12e81b84df2ddc323ceefc2a724b20a485bf8
Delsart <d2289582155@gmail.com>
2023-05-23 23:53:00 +0800

    first commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,9 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        maven {
+            url = uri("https://jitpack.io")
+        }
         mavenCentral()
     }
 }
